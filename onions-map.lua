8a6ef3570fc88b79516e258585ca2abe8b788153
Nicholas Merrill <nick@calyx.com>
2015-10-14 15:53:11 -0400

    Create onions-map.lua

---
+++ onions-map.lua
@@ -0,0 +1,10 @@
+onions_map = {
+["jabber.calyxinstitute.org"] = "ijeeynrc6x2uy5ob.onion";
+["riseup.net"] = "4cjw6cwpeaeppfqz.onion";
+["jabber.otr.im"] = "5rgdtlawqkcplz75.onion";
+["jabber.systemli.org"] = "x5tno6mwkncu5m3h.onion";
+["securejabber.me"] = "giyvshdnojeivkom.onion";
+["so36.net"] = "s4fgy24e2b5weqdb.onion";
+["jabber.autistici.org"] = "wi7qkxyrdpu5cmvr.onion";
+["jabber.ipredator.se"] = "3iffdebkzzkpgipa.onion";
+};
