86c15376abdf895aa8887a3e01523c569f37a96a
David Goulet <dgoulet@ev0ke.net>
2015-10-16 16:42:01 -0400

    Indent properly the list
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- onions-map.lua
+++ onions-map.lua
@@ -1,14 +1,14 @@
 onions_map = {
-["jabber.calyxinstitute.org"] = "ijeeynrc6x2uy5ob.onion";
-["riseup.net"] = "4cjw6cwpeaeppfqz.onion";
-["jabber.otr.im"] = "5rgdtlawqkcplz75.onion";
-["jabber.systemli.org"] = "x5tno6mwkncu5m3h.onion";
-["securejabber.me"] = "giyvshdnojeivkom.onion";
-["so36.net"] = "s4fgy24e2b5weqdb.onion";
-["autistici.org"] = "wi7qkxyrdpu5cmvr.onion";
-["inventati.org"] = "wi7qkxyrdpu5cmvr.onion";
-["jabber.ipredator.se"] = "3iffdebkzzkpgipa.onion";
-["cloak.dk"] = "m2dsl4banuimpm6c.onion";
-["im.koderoot.net"] = "ihkw7qy3tok45dun.onion";
-["anonymitaet-im-inter.net"] = "rwf5skuv5vqzcdit.onion";
+	["jabber.calyxinstitute.org"] = "ijeeynrc6x2uy5ob.onion";
+	["riseup.net"] = "4cjw6cwpeaeppfqz.onion";
+	["jabber.otr.im"] = "5rgdtlawqkcplz75.onion";
+	["jabber.systemli.org"] = "x5tno6mwkncu5m3h.onion";
+	["securejabber.me"] = "giyvshdnojeivkom.onion";
+	["so36.net"] = "s4fgy24e2b5weqdb.onion";
+	["autistici.org"] = "wi7qkxyrdpu5cmvr.onion";
+	["inventati.org"] = "wi7qkxyrdpu5cmvr.onion";
+	["jabber.ipredator.se"] = "3iffdebkzzkpgipa.onion";
+	["cloak.dk"] = "m2dsl4banuimpm6c.onion";
+	["im.koderoot.net"] = "ihkw7qy3tok45dun.onion";
+	["anonymitaet-im-inter.net"] = "rwf5skuv5vqzcdit.onion";
 };
